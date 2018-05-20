0c553be74f45eb4e73ed37aca400fb37a5ab4b4c
CarlosVillavicencio <carlos.villavicencio.g7@gmail.com>
2018-05-20 01:55:07 -0500

    crud usuarios(edit grupos)

--- src/AppBundle/Repository/FosUserRepository.php
+++ src/AppBundle/Repository/FosUserRepository.php
@@ -29,4 +29,37 @@ class FosUserRepository extends EntityRepository
             ->getResult(2);
         return $array[0];
     }
+
+    public function obtenerGruposYUsuarios($user_id = null)
+    {
+        if ($user_id != null) {
+            $array = $this->getEntityManager()
+                ->createQueryBuilder()
+                ->select(
+                    'fg.id group_id',
+                    'fg.name',
+                    'fu.id user_id'
+                )
+                ->from('AppBundle:FosGroup', 'fg')
+                ->join('fg.user', 'fu', 'WITH', 'fu.id = :user_id')
+                ->setParameter('user_id', $user_id)
+                ->orderBy('fg.id')
+                ->getQuery()
+                ->getResult(2);
+        } else {
+            $array = $this->getEntityManager()
+                ->createQueryBuilder()
+                ->select(
+                    'fg.id group_id',
+                    'fg.name',
+                    'fu.id user_id'
+                )
+                ->from('AppBundle:FosGroup', 'fg')
+                ->leftJoin('fg.user', 'fu')
+                ->orderBy('fg.id')
+                ->getQuery()
+                ->getResult(2);
+        }
+        return $array;
+    }
 }
--- src/desarrollo/DashboardBundle/Controller/UsuariosController.php
+++ src/desarrollo/DashboardBundle/Controller/UsuariosController.php
@@ -2,6 +2,7 @@
 
 namespace desarrollo\DashboardBundle\Controller;
 
+use AppBundle\AppBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\FosUser;
@@ -30,9 +31,11 @@ class UsuariosController extends Controller
         if (!is_array($data['roles'])) {
             $data['roles'] = unserialize($data['roles']);
         }
+        $grupos = $this->getDoctrine()->getRepository(FosUser::class)->obtenerGruposYUsuarios();
         return $this->render(':Dashboard/Usuarios:editar.html.twig', array(
             'id' => $id,
-            'data' => $data
+            'data' => $data,
+            'grupos' => $grupos
         ));
     }
 
@@ -82,28 +85,42 @@ class UsuariosController extends Controller
 
     public function updateAction(Request $request, $id)
     {
+//        canonical de fosuserbundle se consigue de la siguiente forma
+//        dump(mb_convert_case($request->request->get('email'), MB_CASE_LOWER, "UTF-8"));
         $tools = $this->get('herramientas');
         if (!$request->isXmlHttpRequest()) {
             return $tools->redirectToHome();
         }
         try {
             $em = $this->getDoctrine()->getManager();
-            $grupo = $em->getRepository(FosUser::class)->find($id);
             $array = array(
                 'status' => true,
                 'msg' => $tools->trans('genericos.consultas.reg_modificado')
             );
-            if (!$grupo) {
+            $userManager = $this->get('fos_user.user_manager');
+            $usuario = $userManager->findUserBy(array('id' => $id));
+            if (!$usuario) {
                 $array['status'] = false;
                 $array['msg'] = $tools->trans('genericos.consultas.no_se_encontro');
                 return $tools->toastDanger($array['msg']);
             }
-            $name = $request->request->get('name');
-            $roles = strtoupper($request->request->get('roles'));
-            $roles = explode(',', $roles);
-            $roles = serialize($roles);
-            $grupo->setName($name);
-            $grupo->setRoles($roles);
+            $data = $request->request->all();
+            dump($data);
+            exit();
+//            user_group
+            $usuario->setUsername($data['username']);
+            $usuario->setUsernameCanonical($data['username']);
+            $usuario->setEmail($data['email']);
+            $usuario->setEmailCanonical($data['email']);
+            if ($request->request->get('password', '') != '') {
+                $usuario->setPlainPassword($data['password']);
+            }
+            $userManager->updateUser($usuario);
+            $usuario = $em->getRepository(FosUser::class)->find($id);
+            $usuario->setThemeDashboard($data['themeDashboard']);
+            $usuario->setFirstName($data['firstName']);
+            $usuario->setLastName($data['lastName']);
+            $usuario->setEnabled($request->request->get('enabled', false));
             $em->flush();
             return $tools->jsonResponse($array);
         } catch (\Exception $exc) {
